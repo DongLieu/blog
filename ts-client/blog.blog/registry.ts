f1de7abada1501db19ff220b4d941c5bfee29a94
DongLieu <duongduydongtq@gmail.com>
2022-11-11 18:30:56 +0700

    taoj laij

--- ts-client/blog.blog/registry.ts
+++
@@ -1,9 +0,0 @@
-import { GeneratedType } from "@cosmjs/proto-signing";
-import { MsgCreatePost } from "./types/blog/tx";
-
-const msgTypes: Array<[string, GeneratedType]>  = [
-    ["/blog.blog.MsgCreatePost", MsgCreatePost],
-    
-];
-
-export { msgTypes }
--- ts-client/blog.blog/types.ts
+++
@@ -1,7 +0,0 @@
-import { Params } from "./types/blog/params"
-
-
-export {     
-    Params,
-    
- }
